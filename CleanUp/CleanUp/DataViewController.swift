cbcd4c961feba986605905f36c3aedbd96e4553f
Cody Skala <cws26@zips.uakron.edu>
2015-11-03 20:12:59 -0500

    Original default of PageBaseApp

---
+++ CleanUp/CleanUp/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  CleanUp
+//
+//  Created by Skala,Cody on 11/3/15.
+//  Copyright © 2015 Skala,Cody. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
